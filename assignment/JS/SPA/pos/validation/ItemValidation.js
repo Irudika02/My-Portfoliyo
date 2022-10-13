a88af0b5a51c9a8a237377c65f8de501aa8bd8ff
Irudika02 <iriyaaa12345@gmail.com>
2022-10-13 13:13:40 +0530

    itemname updated

--- assignment/JS/SPA/pos/validation/ItemValidation.js
+++ assignment/JS/SPA/pos/validation/ItemValidation.js
@@ -45,3 +45,8 @@ $("#txtItemCode").on('keydown', function (event) {
         focusText($("#txtItemCode"));
     }
 });
+$("#txtItemName").on('keydown', function (event) {
+    if (event.key == "Enter" && check(itemNameRegEx, $("#txtItemName"))) {
+        focusText($("#txtItemQty"));
+    }
+});
