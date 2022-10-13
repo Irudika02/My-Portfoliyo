f93a40a51b2f3dbd4864d06915166294b6b929a2
Irudika02 <iriyaaa12345@gmail.com>
2022-10-13 10:17:20 +0530

    content update

---
+++ assignment/JS/SPA/pos/validation/CustomerValidation.js
@@ -0,0 +1,7 @@
+$("#txtCustomerID").focus();
+
+const cusIDRegEx = /^(C00-)[0-9]{1,3}$/;
+const cusNameRegEx = /^[A-z ]{5,20}$/;
+const cusAddressRegEx = /^[0-9/A-z. ,]{7,}$/;
+const cusContactRegEx = /^[\\+]?[(]?[0-9]{3}[)]?[-\\s\\.]?[0-9]{3}[-\\s\\.]?[0-9]{4,6}$/;
+const cusSalaryRegEx = /^[0-9]{1,}[.]?[0-9]{1,2}$/;
