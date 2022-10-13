a241f28ff452dff0fe792795b6b5c99743391870
Irudika02 <iriyaaa12345@gmail.com>
2022-10-13 09:09:24 +0530

    'rr'

--- assignment/JS/SPA/pos/controller/CustomerController.js
+++ assignment/JS/SPA/pos/controller/CustomerController.js
@@ -16,3 +16,20 @@ $("#saveCustomer").click(function () {
         salary: customerSalary
     }
 });
+
+function loadAllCustomers() {
+    $("#tblCustomer").empty();
+
+    for (var customer of customers) {
+
+        var all = `<tr><td>${customer.id}</td><td>${customer.name}</td><td>${customer.address}</td><td>${customer.contact}</td><td>${customer.salary}</td>
+                        <td><button class="btn btn-warning btn-mini" data-bs-target="#editCustomers"
+                        data-bs-toggle="modal" id="btn-edit"><i class="fa-solid fa-pen-to-square"></i> Edit
+                        </button>
+                        <button class="btn btn-danger btn-mini delete"><i class="fa-solid fa-trash"></i> Delete</button>
+                        </td>
+                    </tr>`;
+
+        $("#tblCustomer").append(all);
+    }
+}
