92aeabc2908267e30fe8256010d6918297d1a3da
Irudika02 <iriyaaa12345@gmail.com>
2022-10-13 09:24:22 +0530

    added content

---
+++ assignment/JS/SPA/pos/controller/PurchaseOrderController.js
@@ -0,0 +1,12 @@
+function loadAllCustomersForOption() {
+    $("#selectCustomerID").empty();
+    for (let cus of customers) {
+        $("#selectCustomerID").append(`<option>${cus.id}</option>`);
+    }
+
+}
+$("#selectCustomerID").click(function (){
+    let id = $("#selectCustomerID").val();
+    let search = searchCustomer(id);
+    $("#orderCustomerName").val(search.name);
+});
