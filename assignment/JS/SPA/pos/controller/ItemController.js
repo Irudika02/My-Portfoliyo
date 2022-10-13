bcd8d62a03bbfea74fce5a035ebdf001433f643d
Irudika02 <iriyaaa12345@gmail.com>
2022-10-13 12:02:55 +0530

    added item content

---
+++ assignment/JS/SPA/pos/controller/ItemController.js
@@ -0,0 +1,16 @@
+$("#saveItem").click(function (){
+
+    let itemCode = $("#txtItemCode").val();
+    let itemName = $("#txtItemName").val();
+    let qty = $("#txtItemQty").val();
+    let unitPrice = $("#txtItemUnitPrice").val();
+
+
+    var itemObject = {
+        code: itemCode,
+        itemName: itemName,
+        qty: qty,
+        unitPrice: unitPrice,
+
+    }
+});
