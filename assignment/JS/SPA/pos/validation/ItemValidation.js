5def5d5102737d3262f6e1c644962289355fc257
Irudika02 <iriyaaa12345@gmail.com>
2022-10-13 13:14:33 +0530

    itemqty updated

--- assignment/JS/SPA/pos/validation/ItemValidation.js
+++ assignment/JS/SPA/pos/validation/ItemValidation.js
@@ -50,3 +50,8 @@ $("#txtItemName").on('keydown', function (event) {
         focusText($("#txtItemQty"));
     }
 });
+$("#txtItemQty").on('keydown', function (event) {
+    if (event.key == "Enter" && check(itemQtyRegEx, $("#txtItemQty"))) {
+        focusText($("#txtItemUnitPrice"));
+    }
+});
