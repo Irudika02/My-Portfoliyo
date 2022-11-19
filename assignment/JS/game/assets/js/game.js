e73fd32b476bee1ffc78962e4d66d47db3bb1001
Irudika02 <iriyaaa12345@gmail.com>
2022-11-19 08:27:53 +0530

    added run animation

--- assignment/JS/game/assets/js/game.js
+++ assignment/JS/game/assets/js/game.js
@@ -2,6 +2,8 @@ var boy=document.getElementById("boy");
 idleImageNumber =0;
 idleAnimationNumber =0;
 
+
+
 function idleAnimation(){
     idleImageNumber = idleImageNumber +1;
     if (idleImageNumber == 16){
@@ -12,4 +14,29 @@ function idleAnimation(){
 }
 function idleAnimationStart(){
     idleAnimationNumber = setInterval(idleAnimation,200);
+}
+
+
+
+
+
+runImageNumber =0;
+runAnimationNumber=0;
+
+function runAnimation() {
+    runImageNumber = runImageNumber+1;
+
+    if (runImageNumber == 16){
+        runImageNumber =1;
+    }
+
+    boy.src="assets/image/boy/run("+runImageNumber+").png";
+
+}
+function runAnimationStart(){
+    runAnimationNumber = setInterval(runAnimation,200);
+    clearInterval(idleAnimationNumber);
+}
+function keyCheck(event){
+    alert(event.which);
 }
